270b770dc175f1b8df256c27ab6811001a6544f4
end user <kreimendahlf@wit.edu>
2017-10-31 12:39:40 -0400

    added skeleton code

---
+++ src/edu/wit/cs/comp1000/PA7a.java
@@ -0,0 +1,44 @@
+package edu.wit.cs.comp1000;
+
+import java.util.Scanner;
+
+//TODO: document this class
+public class PA7a {
+	
+	/**
+	 * Reads all values from the provided scanner
+	 * into the supplied array (up to its size)
+	 * and returns the number of integers read
+	 * 
+	 * @param input input source
+	 * @param nums destination for read integers
+	 * @return number of integers read
+	 */
+	public static int readIntoArray(Scanner input, int[] nums) {
+		return 0; // TODO: replace with your code
+	}
+	
+	/**
+	 * Prints to the screen the average of the supplied array,
+	 * up to the supplied size, and all integers in the array, 
+	 * again up to the supplied size, that are larger than the average
+	 * 
+	 * @param nums array of numbers
+	 * @param size number of valid elements
+	 */
+	public static void printAboveAverage(int[] nums, int size) {
+		// TODO: write your code here
+	}
+
+	/**
+	 * Program execution point:
+	 * input a sequence of integers (up to 100),
+	 * output average of integers and those over the average
+	 * 
+	 * @param args command-line arguments (ignored)
+	 */
+	public static void main(String[] args) {
+		// TODO: write your code here
+	}
+
+}
---
+++ src/edu/wit/cs/comp1000/PA7b.java
@@ -0,0 +1,18 @@
+package edu.wit.cs.comp1000;
+
+//TODO: document this class
+public class PA7b {
+
+	/**
+	 * Program execution point:
+	 * input text via console input,
+	 * output counts for each letter
+	 * found in the input (case-insensitive)
+	 * 
+	 * @param args command-line arguments (ignored)
+	 */
+	public static void main(String[] args) {
+		// TODO: write your code here
+	}
+
+}
